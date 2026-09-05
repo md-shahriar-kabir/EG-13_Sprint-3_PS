53803f087b5529cca058e07390aa9316827ff170
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-09-05 20:49:37 +0600

    2nd problem solved

--- index.js
+++ index.js
@@ -18,3 +18,30 @@ var containsDuplicate = function(nums) {
     
     return false;
 };
+
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+
+
+// 02. Move Zeroes
+// Write a transformation function that moves all zero values to the end of an array while maintaining the relative order of the non-zero elements.
+
+// /**
+// * @param {number[]} nums
+// * @return {void} Do not return anything, modify nums in-place instead.
+//  */
+
+var moveZeroes = function(nums) {
+    let lastNonZeroFoundAt = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[lastNonZeroFoundAt] = nums[i];
+            lastNonZeroFoundAt++;
+        }
+    }
+
+    for (let i = lastNonZeroFoundAt; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+};
