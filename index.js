39e356af40039287acc2a9ce11b96ca6b1f72cf2
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-09-05 20:57:35 +0600

    9th problem solved

--- index.js
+++ index.js
@@ -251,3 +251,38 @@ var topKFrequent = function(nums, k) {
 
     return result.slice(0, k);
 };
+
+// --------------------------------------------------------------------
+// --------------------------------------------------------------------
+
+// 09. Longest Consecutive Sequence
+// Write a function that finds the length of the longest sequence of consecutive integers in an unsorted array. The solution should aim for linear time complexity.
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+
+var longestConsecutive = function(nums) {
+    if (nums.length === 0) return 0;
+
+    const numSet = new Set(nums);
+    let longestStreak = 0;
+
+    for (let num of numSet) {
+
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;
+};
